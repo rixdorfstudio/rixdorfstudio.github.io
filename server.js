2d7e2a794677219027c62b6c7aed6fe50ddf05a2
Bruno <brunoherzog@web.de>
2026-06-04 09:48:56 +0200

    Add Sudoku Roza: support page + update website to show as live

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 8765;
+const dir = __dirname;
+
+const mime = { '.html': 'text/html', '.css': 'text/css', '.js': 'application/javascript', '.png': 'image/png', '.jpg': 'image/jpeg', '.svg': 'image/svg+xml' };
+
+http.createServer((req, res) => {
+  let filePath = path.join(dir, req.url === '/' ? 'index.html' : req.url);
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': mime[path.extname(filePath)] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(port, () => console.log('Serving on ' + port));
